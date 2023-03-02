0b0534ccf8e0f722c2812c86198cb2899470cc95
Freddy Sandoval <fjsandoval11@gmail.com>
2023-03-01 23:11:34 -0500

    create joe portfolio and add hoem page

---
+++ src/Pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className='about'>
+            <h2>Reliable handyman and home repair services</h2>
+            <div className='prompt'>
+                <p>
+                    Need something installed or repaired? Call Joe!
+                </p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
