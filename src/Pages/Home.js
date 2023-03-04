cab1de253e93f656e24ced531db1e2e5dfe3e8e3
Freddy Sandoval <fjsandoval11@gmail.com>
2023-03-04 13:05:06 -0500

    added margins for better centering of div boxes and fixed homepage content layout

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,61 +11,66 @@ const Home = () => {
         <div className="prompt">
           <p>Need something installed or repaired? Call Joe!</p>
         </div>
-
-        <div className="serviceBox">
-
-            {/* <div className="serviceTitle">
-
-          <h1>Services</h1>
-            </div> */}
-
-        <div className="services1">
-            <span>
-            <span>Painting </span>
-            <br/>
-            <span>Drywall and plaster repair </span>
-            <br/>
-            <span>Door and window repair </span>
-            <br/>
-            <span>Gutter repair </span>
-            <br/>
-            <span>Gutter cleaning </span>
-            <br/>
-            <span>Storm door installation</span>
-            <br/>
-            <span>Closet door installation</span>
-            </span>
-            
-        </div>
-
-        <div className="imageContainer">
-        
-         <img src={handyMan} />     
-        </div>
-
-        <div className="services2">
-
-            <span>
-            <span>Door locks and deadbolts</span>
-            <br/>
-            <span>Bathroom exhaust and fan installation</span>
-            <br/>
-            <span>Interior & exterior door installation</span>
-            <br/>
-            <span>Blinds & window treatment installation</span>
-            <br/>
-            <span>Closet shelving desgin & installation</span>
-            <br/>
-            <span>Wall shelving design and installation</span>
-            <br/>
-            <span>Storage solutions and shelving</span>
-            </span>
-
-        </div>
-
-        </div>
       </div>
+
+
+      <div className="serviceBox">
+
+{/* <div className="serviceTitle">
+
+<h1>Services</h1>
+</div> */}
+
+<div className="services1">
+<span>
+<span>Painting </span>
+<br/>
+<span>Drywall and plaster repair </span>
+<br/>
+<span>Door and window repair </span>
+<br/>
+<span>Gutter repair </span>
+<br/>
+<span>Gutter cleaning </span>
+<br/>
+<span>Storm door installation</span>
+<br/>
+<span>Closet door installation</span>
+</span>
+
+</div>
+
+<div className="imageContainer">
+
+<img src={handyMan} />     
+</div>
+
+<div className="services2">
+
+<span>
+<span>Door locks and deadbolts</span>
+<br/>
+<span>Bathroom exhaust and fan installation</span>
+<br/>
+<span>Interior & exterior door installation</span>
+<br/>
+<span>Blinds & window treatment installation</span>
+<br/>
+<span>Closet shelving desgin & installation</span>
+<br/>
+<span>Wall shelving design and installation</span>
+<br/>
+<span>Storage solutions and shelving</span>
+</span>
+
+</div>
+
+</div>
+
     </div>
+    
+
+    
   );
 };
 
