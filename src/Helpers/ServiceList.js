b6dd243c44e1e596edddf316168be14b5dcf5bd0
Freddy Sandoval <fjsandoval11@gmail.com>
2023-03-08 01:26:52 -0500

    cleaned up homepage code with serviceList helper and adjusted styling for new UL tag

---
+++ src/Helpers/ServiceList.js
@@ -0,0 +1,49 @@
+
+
+export
+
+const serviceList2 = [
+`Hard to reach lightbulbs `,
+'Ceiling fan installation',
+'Light switches',
+'Outlet replacement',
+'Kitchen repairs',
+'Pet door installation',
+'Picture hanging',
+'Mailbox installation',
+'Siding repair',
+'TV wall-mounting',
+'Fascia roofing repair',
+'Soffit roofing repair',
+'Tile repair',
+'Tile flooring',
+'Wood flooring',
+'Laminate flooring',
+'Bamboo flooring',
+'Floating flooring',
+'Furniture assembly'
+]
+
+export const serviceList1 = [
+'Painting',
+'Drywall and plaster repair',
+'Door and window trim',
+'Gutter repair ',
+'Gutter cleaning',
+'Storm door installation',
+'Closet door installation',
+'Door locks and deadbolts',
+'Bathroom exhaust fan installation',
+'Interior & exterior door installation',
+'Blinds & window treatment installation',
+'Closet shelving design & installation',
+'Wall shelving design and installation',
+'Storage solutions and shelving',
+'Carbon monoxide detector replacement',
+'Smoke detector replacement',
+'Lighting fixture replacement',
+'Bathroom tub and shower resealing'
+
+]
+
+
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,98 +1,45 @@
 import React from "react";
+import { serviceList2, serviceList1 } from "../Helpers/ServiceList";
 import "../Styles/Home.css";
-import handyMan from '../Assets/handyMan.png'
-
-
-
+import handyMan from "../Assets/handyMan.png";
 
 const Home = () => {
   return (
     <div className="home">
-
       <div className="aboutContainer">
-
-      <span className="title">
-        <h1>Cruz HandyMan Services</h1>   
-      </span>
+        <span className="title">
+          <h1>Cruz HandyMan Services</h1>
+        </span>
 
         <span className="prompt">
           <p>Need something installed or repaired? Call Joe at: </p>
 
-         <p className="phone"> 978-770-3287 </p>
-         
+          <p className="phone"> 978-770-3287 </p>
         </span>
       </div>
 
+      <div className="serviceBox">
+        <div className="services1">
+          <span>
+            {serviceList1.map((service) => (
+              <ul>{service}</ul>
+            ))}
+          </span>
+        </div>
 
- <div className="serviceBox">
+        <div className="imageContainer">
+          <img src={handyMan} />
+        </div>
 
-{/* <div className="serviceTitle">
-
-<h1>Services</h1>
-</div> */}
-
-<div className="services1">
-<span>
-<span>Painting </span>
-<br/>
-<span>Drywall and plaster repair </span>
-<br/>
-<span>Door and window repair </span>
-<br/>
-<span>Gutter repair </span>
-<br/>
-<span>Gutter cleaning </span>
-<br/>
-<span>Storm door installation</span>
-<br/>
-<span>Closet door installation</span>
-<br />
-<span>Door locks and deadbolts</span>
-<br/>
-<span>Bathroom exhaust and fan installation</span>
-<br/>
-<span>Interior & exterior door installation</span>
-<br/>
-<span>Blinds & window treatment installation</span>
-<br/>
-<span>Closet shelving desgin & installation</span>
-<br/>
-<span>Wall shelving design and installation</span>
-<br/>
-<span>Storage solutions and shelving</span>
-<br />
-<span>Carbon monoxide detector replacement</span>
-<br />
-<span>Smoke detector replacement</span>
-<br />
-<span>lighting fixture replacement</span>
-<br />
-<span>Bathroom tub and shower resealing</span>
-</span>
-
-
-
-</div>
-
-<div className="imageContainer">
-
-<img src={handyMan} />     
-</div>
-
-<div className="services2">
-
-<span>
-<span>Hard to reach lightbulbs</span>
-</span>
-
-</div>
-
-</div>
-
+        <div className="services2">
+          <span>
+            {serviceList2.map((service) => (
+              <ul>{service}</ul>
+            ))}
+          </span>
+        </div>
+      </div>
     </div>
-    
-
-    
   );
 };
 
