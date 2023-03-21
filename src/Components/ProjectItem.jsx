8782e29915d09f07efa6e8dffaee0da6cc97c6d2
Freddy Sandoval <fjsandoval11@gmail.com>
2023-03-20 23:31:01 -0400

    added componenet for project page

---
+++ src/Components/ProjectItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const ProjectItem = () => {
+  return (
+    <div>
+        <div style={{backgroundImage: `url(${image}`}} className='bgImage'
+        onClick={() => {navigate('/project' + id)}} />
+            
+      
+    </div>
+  )
+}
+
+export default ProjectItem
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,6 +5,9 @@ const Projects = () => {
   return (
       <div className='projects'>
       <h1> Our Work </h1>
+      <div className='projectList'>
+      
+      </div>
     
     </div>
   )
